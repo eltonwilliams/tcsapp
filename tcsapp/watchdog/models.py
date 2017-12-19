1f972461960a9baa1188af916a8d4a5f13aa9d7b
Elton Williams <EWilliams@melbro.co.za>
2017-12-19 16:34:58 +0200

    added test data and template

--- tcsapp/watchdog/models.py
+++ tcsapp/watchdog/models.py
@@ -4,19 +4,19 @@ from django.utils import timezone
 
 class Store(models.Model):
     code = models.CharField(max_length=2 , primary_key=True)
-    name = models.TextField()
+    name = models.CharField(max_length=20)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    area_manager = models.TextField()
-    area_manager_tel = models.TextField()
-    store_manager = models.TextField()
-    store_manager_tel = models.TextField()
-    trainee_manager = models.TextField()
-    trainee_manager_tel = models.TextField()
+    area_manager = models.CharField(max_length=40)
+    area_manager_tel = models.CharField(max_length=15)
+    store_manager = models.CharField(max_length=40)
+    store_manager_tel = models.CharField(max_length=15)
+    trainee_manager = models.CharField(max_length=40)
+    trainee_manager_tel = models.CharField(max_length=15)
 
-    user = models.ForeignKey('auth.User')   
-    call_date = models.DateTimeField(blank=True, null=True)  
-    call_comment = models.TextField()
+   # user = models.ForeignKey('auth.User')   
+   # call_date = models.DateTimeField(blank=True, null=True)  
+   # call_comment = models.TextField()
    
 
     def called(self):
@@ -24,4 +24,4 @@ class Store(models.Model):
         self.save()
 
     def __str__(self):
-        return self.code
+        return self.code+' - '+self.name
